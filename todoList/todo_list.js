c4b0d7749217a2ee654ad0e2ac8b5143dd5b1a15
Ricardo Saldarriaga <ricardosaldarriaga2000@gmail.com>
2025-01-20 22:26:29 -0500

    TODOList Practice Task

--- todoList/todo_list.js
+++ todoList/todo_list.js
@@ -3,9 +3,11 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+const clearAllBtn = document.getElementById("clearAllBtn");
 
 addTaskBtn.addEventListener("click", addTask);
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+clearAllBtn.addEventListener("click", clearAllTasks);
 
 let tasks = [];
 
@@ -36,5 +38,10 @@ function toggleTask(index) {
 
 function clearCompletedTasks() {
     tasks = tasks.filter(task => !task.completed);
+    displayTasks();
+}
+
+function clearAllTasks() {
+    tasks = [];
     displayTasks();
 }
