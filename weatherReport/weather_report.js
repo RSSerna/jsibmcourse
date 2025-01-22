d35e6c03a8e6db3c55a589c428060ac39f78780a
Ricardo Saldarriaga <ricardosaldarriaga2000@gmail.com>
2025-01-21 20:54:38 -0500

    Weather Report Practice Task

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,23 +1,33 @@
-function showweatherDetails(event) {
+const weatherK = 'f369fff6c9adea429a2effe82080bfcc';
+function showWeatherFromCoordinates(event) {
     event.preventDefault();
     const lat = document.getElementById('lat').value;
     const lon = document.getElementById('lon').value;
-    const weatherK = 'f369fff6c9adea429a2effe82080bfcc';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherK}`;
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            const weatherInfo = document.getElementById('weatherInfo');
-            weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
-                                <p>Temperature: ${data.main.temp} &#8451;</p>
-                                <p>Weather: ${data.weather[0].description}</p>`;
-        }).catch(error => {
-            console.error('Error fetching weather:', error);
-            const weatherInfo = document.getElementById('weatherInfo');
-            weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
-        });
+    showData(apiUrl);
 }
 
+function showWeatherByName(event) {
+    event.preventDefault();
+    const city = document.getElementById('city').value;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherK}`;
+    showData(apiUrl);
+}   
 
+function showData(url){
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                            <p>Temperature: ${data.main.temp} &#8451;</p>
+                            <p>Weather: ${data.weather[0].description}</p>`;
+    }).catch(error => {
+        console.error('Error fetching weather:', error);
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+    });
+}
 
-document.getElementById('weatherForm').addEventListener('submit', showweatherDetails);
+document.getElementById('weatherFromCoordinateForm').addEventListener('submit', showWeatherFromCoordinates);
+document.getElementById('weatherByNameForm').addEventListener('submit', showWeatherByName);
